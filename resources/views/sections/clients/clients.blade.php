9588176edf006e5ac6e3b0def99e081b389956ee
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2020-09-02 18:04:17 +0300

    Common layout

---
+++ resources/views/sections/clients/clients.blade.php
@@ -0,0 +1,13 @@
+<section class="page_section" id="clients">
+    <h2>Clients</h2>
+    <div class="client_logos">
+        <div class="container">
+            <ul class="fadeInRight animated wow">
+                <li><a href="javascript:void(0)"><img src="img/client_logo1.png" alt=""></a></li>
+                <li><a href="javascript:void(0)"><img src="img/client_logo2.png" alt=""></a></li>
+                <li><a href="javascript:void(0)"><img src="img/client_logo3.png" alt=""></a></li>
+                <li><a href="javascript:void(0)"><img src="img/client_logo5.png" alt=""></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
